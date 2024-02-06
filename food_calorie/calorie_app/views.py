35009e284fc42b4842167448d0372cd02abb5628
dileep <dileepmohandas14@gmail.com>
2024-02-06 20:19:52 +0530

    updated html

--- food_calorie/calorie_app/views.py
+++ food_calorie/calorie_app/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 
+
+#RHaV4fHDyCr3tPYFLC1Pug==1mNJOflVQ7eXvf13
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    import json
+    import requests
+    if request.method =="POST":
+
+        query = request.POST['query']
+        api_url = 'https://api.api-ninjas.com/v1/nutrition?query='
+        api_request = requests.get(api_url + query, headers={'X-Api-Key': 'RHaV4fHDyCr3tPYFLC1Pug==1mNJOflVQ7eXvf13'})
+        try:
+            api = json.loads(api_request.content)
+            print(api_request.content)
+        except Exception as e:
+            api = "oops! There was an error"
+            print(e)
+        return render(request, 'home.html',{'api':api})
+    else:
+        return render(request, 'home.html',{'query':'Enter a valid query'})
+
+
